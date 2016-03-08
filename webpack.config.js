d766e42276b80c0ccd4fb22c93daebe587dbfc93
havefive <lzc09008@gmail.com>
2016-03-08 14:38:49 +0800

    gulp webpack init

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: lizhaocai
+ * @Date:   2016-02-28 14:16:41
+ * @Last Modified by:   lizhaocai
+ * @Last Modified time: 2016-03-04 14:24:50
+ */
+
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'build/app.js'
+    }
+};
