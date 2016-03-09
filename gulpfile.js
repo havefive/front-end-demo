af27e34cff97a73ca3e29997d16c685b11ca6cc3
havefive <lzc09008@gmail.com>
2016-03-09 16:18:19 +0800

    +postcss

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,16 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var run = require('run-sequence');
 
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnext = require('cssnext');
+var precss = require('precss');
 
 // gulp 帮助命令说明
 gulp.task('help', function () {
-
-    console.log('	gulp browser-sync			启动浏览器,自动刷新');
     console.log('	gulp help			        gulp参数说明');
+    console.log('	gulp browser-sync			启动浏览器,自动刷新');
+    console.log('	gulp css			        postcss处理生成css');
 
 });
 
@@ -24,6 +28,18 @@ gulp.task('browser-sync', function() {
         },
         port:8080
     });
+});
+
+// postcss预处理生成css
+gulp.task('css', function () {
+    var processors = [
+        autoprefixer,
+        cssnext,
+        precss
+    ];
+    return gulp.src('./src/css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./build'));
 });
 
 // 默认task,gulp 命令
